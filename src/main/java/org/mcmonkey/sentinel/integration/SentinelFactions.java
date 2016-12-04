1a73389fcde3b588633a88a6fd51fe43bdae1f27
mcmonkey4eva <mcmonkey4eva@hotmail.com>
2016-12-03 20:58:36 -0800

    Fix a typo
    Why did this compile fine without this fix

--- src/main/java/org/mcmonkey/sentinel/integration/SentinelFactions.java
+++ src/main/java/org/mcmonkey/sentinel/integration/SentinelFactions.java
@@ -22,7 +22,7 @@ public class SentinelFactions extends SentinelIntegration {
                 String factionName = text.substring("factions:".length());
                 Faction faction = FactionColl.get().getByName(factionName);
                 for (MPlayer pl: faction.getMPlayers()) {
-                    if (pl.getPlayer() != null & pl.getPlayer().getUniqueId() != null
+                    if (pl.getPlayer() != null && pl.getPlayer().getUniqueId() != null
                             && pl.getPlayer().getUniqueId().equals(ent.getUniqueId())) {
                         return true;
                     }
