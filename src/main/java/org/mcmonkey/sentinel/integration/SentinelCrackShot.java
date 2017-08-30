d01ea9027d6bf73be438cdd2f490beb97b9f9f73
mcmonkey4eva <agoodwin@freneticllc.com>
2017-08-30 12:02:01 -0700

    Idea for ammo handling in CrackShot

--- src/main/java/org/mcmonkey/sentinel/integration/SentinelCrackShot.java
+++ src/main/java/org/mcmonkey/sentinel/integration/SentinelCrackShot.java
@@ -3,6 +3,7 @@ package org.mcmonkey.sentinel.integration;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 import org.mcmonkey.sentinel.SentinelIntegration;
 import org.mcmonkey.sentinel.SentinelTrait;
 import com.shampaggon.crackshot.CSDirector;
@@ -25,7 +26,9 @@ public class SentinelCrackShot extends SentinelIntegration {
             return false;
         }
         st.faceLocation(ent.getEyeLocation());
+        ItemStack itm = ((Player) st.getLivingEntity()).getItemInHand();
         direc.csminion.weaponInteraction((Player) st.getLivingEntity(), node, false);
+        ((Player) st.getLivingEntity()).setItemInHand(itm);
         if (st.rangedChase) {
             st.rechase();
         }
