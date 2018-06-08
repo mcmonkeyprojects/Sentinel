7745b1045c88279b2acd61a586216ebf51aace16
mcmonkey <mcmonkey4eva@hotmail.com>
2018-06-08 14:01:08 -0700

    Accuracy implementation experiment for #236

--- src/main/java/org/mcmonkey/sentinel/integration/SentinelCrackShot.java
+++ src/main/java/org/mcmonkey/sentinel/integration/SentinelCrackShot.java
@@ -4,6 +4,7 @@ import org.bukkit.Bukkit;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 import org.mcmonkey.sentinel.SentinelIntegration;
 import org.mcmonkey.sentinel.SentinelTrait;
 import com.shampaggon.crackshot.CSDirector;
@@ -25,7 +26,12 @@ public class SentinelCrackShot extends SentinelIntegration {
         if (node == null) {
             return false;
         }
-        st.faceLocation(ent.getEyeLocation());
+        Vector faceAcc = ent.getEyeLocation().toVector().subtract(st.getLivingEntity().getEyeLocation().toVector());
+        if (faceAcc.lengthSquared() > 0.0) {
+            faceAcc = faceAcc.normalize();
+        }
+        faceAcc = st.fixForAcc(faceAcc);
+        st.faceLocation(st.getLivingEntity().getEyeLocation().clone().add(faceAcc.multiply(10)));
         ItemStack itm = ((Player) st.getLivingEntity()).getItemInHand();
         direc.csminion.weaponInteraction((Player) st.getLivingEntity(), node, false);
         ((Player) st.getLivingEntity()).setItemInHand(itm);
