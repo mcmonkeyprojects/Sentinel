6a650af3b84187185970e8ced38cefe1e385fe5c
Alex 'mcmonkey' Goodwin <agoodwin@freneticllc.com>
2019-08-13 20:15:42 -0700

    add "nation:" target for towny, fixes #308

--- src/main/java/org/mcmonkey/sentinel/integration/SentinelTowny.java
+++ src/main/java/org/mcmonkey/sentinel/integration/SentinelTowny.java
@@ -1,5 +1,6 @@
 package org.mcmonkey.sentinel.integration;
 
+import com.palmergames.bukkit.towny.object.Nation;
 import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.object.TownyUniverse;
 import org.bukkit.entity.LivingEntity;
@@ -10,12 +11,12 @@ public class SentinelTowny extends SentinelIntegration {
 
     @Override
     public String getTargetHelp() {
-        return "towny:TOWN_NAME";
+        return "towny:TOWN_NAME, nation:NATION_NAME";
     }
 
     @Override
     public String[] getTargetPrefixes() {
-        return new String[] { "towny" };
+        return new String[] { "towny", "nation" };
     }
 
     @Override
@@ -25,6 +26,18 @@ public class SentinelTowny extends SentinelIntegration {
                 if (TownyUniverse.getDataSource().hasTown(value)) {
                     Town t = TownyUniverse.getDataSource().getTown(value);
                     if (t.hasResident(ent.getName())) { // TODO: Why no UUID support?!
+                        return true;
+                    }
+                }
+                else {
+                    // TODO: Error?
+                    return false;
+                }
+            }
+            else if (prefix.equals("nation") && ent instanceof Player) {
+                if (TownyUniverse.getDataSource().hasNation(value)) {
+                    Nation n = TownyUniverse.getDataSource().getNation(value);
+                    if (n.hasResident(ent.getName())) { // TODO: Why no UUID support?!
                         return true;
                     }
                 }
