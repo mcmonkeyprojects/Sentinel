514e2fabab4788acf163ba7e282707f14f6eef97
fullwall <fullwall@users.noreply.github.com>
2025-09-01 15:17:14 +0800

    Update to 1.21.8. Note: this uses Citizens' setEndermanAngry method instead of reimplementing it which is more future-friendly but does increase dependency on Citizens slightly (#412)

--- src/main/java/org/mcmonkey/sentinel/utilities/SentinelNMSHelper.java
+++ src/main/java/org/mcmonkey/sentinel/utilities/SentinelNMSHelper.java
@@ -16,9 +16,7 @@ public class SentinelNMSHelper {
 
     public static MethodHandle CRAFTENTITY_GETHANDLE, NMSENTITY_WORLDGETTER, NMSWORLD_BROADCASTENTITYEFFECT, NMSENTITY_GETDATAWATCHER, DATWATCHER_SET;
 
-    public static Object ENTITYENDERMAN_DATAWATCHER_ANGRY;
-
-    private static boolean nmsWorks = true, endermanValid = false;
+    private static boolean nmsWorks = true;
 
     public static MethodHandle INVENTORYCLOSEEVENT_GETVIEW, INVENTORYVIEW_GETTITLE;
 
@@ -45,56 +43,31 @@ public class SentinelNMSHelper {
             String bukkitPackageName = Bukkit.getServer().getClass().getPackage().getName();
             Class craftEntity = Class.forName(bukkitPackageName + ".entity.CraftEntity");
             CRAFTENTITY_GETHANDLE = NMS.getMethodHandle(craftEntity, "getHandle", true);
-            Class nmsEntity, nmsWorld, nmsDataWatcher, nmsDataWatcherObject, nmsEntityEnderman, nmsHuman, nmsLivingEntity;
-            String endermanAngryField = null;
+            Class nmsEntity, nmsWorld, nmsDataWatcher, nmsDataWatcherObject;
             String broadcastEffectMethod = "broadcastEntityEffect", dataWatcherSet = "set";
             if (SentinelVersionCompat.v1_17) { // 1.17+ - Mojang mappings update
                 nmsEntity = Class.forName("net.minecraft.world.entity.Entity");
                 nmsWorld = Class.forName("net.minecraft.world.level.World"); // Level
                 nmsDataWatcher = Class.forName("net.minecraft.network.syncher.DataWatcher"); // SynchedEntityData
                 nmsDataWatcherObject = Class.forName("net.minecraft.network.syncher.DataWatcherObject"); // EntityDataAccessor
-                nmsEntityEnderman = Class.forName("net.minecraft.world.entity.monster.EntityEnderman");
                 if (SentinelVersionCompat.v1_21 && !SentinelVersionCompat.vFuture) { // 1.21 names
-                    // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.21.3
-                    if (getOptionalFieldType(nmsEntityEnderman, "cc") == nmsDataWatcherObject) {
-                        endermanAngryField = "cc"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
-                    }
-                    else {
-                        // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.21
-                        endermanAngryField = "ce"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
-                    }
                     broadcastEffectMethod = "a"; // net.minecraft.world.level.Level#broadcastEntityEvent(Entity,byte)
                     dataWatcherSet = "a"; // net.minecraft.network.syncher.SynchedEntityData#set
                 }
                 else if (SentinelVersionCompat.v1_20 && !SentinelVersionCompat.v1_21) { // 1.20 names
-                    try {
-                        // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.20.2
-                        if (getOptionalFieldType(nmsEntityEnderman, "bV") == nmsDataWatcherObject) {
-                            endermanAngryField = "bV"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
-                        }
-                    }
-                    catch (Throwable ex) {
-                        // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.20.6
-                        endermanAngryField = "ca"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
-                    }
                     // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.20.6
                     broadcastEffectMethod = "a"; // net.minecraft.world.level.Level#broadcastEntityEvent(Entity,byte)
                     dataWatcherSet = "a"; // net.minecraft.network.syncher.SynchedEntityData#set
                 }
                 else if (SentinelVersionCompat.v1_19 && !SentinelVersionCompat.v1_20) { // 1.19.4 names
                     // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.19.4
-                    endermanAngryField = "bU"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
                     broadcastEffectMethod = "a"; // net.minecraft.world.level.Level#broadcastEntityEvent(Entity,byte)
                     dataWatcherSet = "b"; // net.minecraft.network.syncher.SynchedEntityData#set
                 }
                 else if (SentinelVersionCompat.v1_18 && !SentinelVersionCompat.v1_19) { // 1.18 names
                     // https://minidigger.github.io/MiniMappingViewer/#/mojang/server/1.18.2
-                    endermanAngryField = "bX"; // net.minecraft.world.entity.monster.EnderMan#DATA_CREEPY
                     broadcastEffectMethod = "a"; // net.minecraft.world.level.Level#broadcastEntityEvent(Entity,byte)
                     dataWatcherSet = "b"; // net.minecraft.network.syncher.SynchedEntityData#set
-                }
-                else if (!SentinelVersionCompat.v1_18) { // 1.17 names
-                    endermanAngryField = "bV"; // EnderMan#DATA_CREEPY
                 }
             }
             else { // 1.12 through 1.16 - Original Spigot NMS versioned mappings
@@ -104,26 +77,15 @@ public class SentinelNMSHelper {
                 nmsWorld = Class.forName(nmsPackageName + ".World");
                 nmsDataWatcher = Class.forName(nmsPackageName + ".DataWatcher");
                 nmsDataWatcherObject = Class.forName(nmsPackageName + ".DataWatcherObject");
-                nmsEntityEnderman = Class.forName(nmsPackageName + ".EntityEnderman");
-                if (SentinelVersionCompat.v1_16) {
-                    endermanAngryField = "bo";
-                }
             }
             NMSENTITY_WORLDGETTER = NMS.getFirstGetter(nmsEntity, nmsWorld);
             NMSENTITY_GETDATAWATCHER = NMS.getFirstGetter(nmsEntity, nmsDataWatcher);
             NMSWORLD_BROADCASTENTITYEFFECT = NMS.getMethodHandle(nmsWorld, broadcastEffectMethod, true, nmsEntity, byte.class);
             DATWATCHER_SET = NMS.getMethodHandle(nmsDataWatcher, dataWatcherSet, true, nmsDataWatcherObject, Object.class);
-            if (endermanAngryField != null && nmsEntityEnderman != null) {
-                Field dataWatcherAngryField = NMS.getField(nmsEntityEnderman, endermanAngryField);
-                dataWatcherAngryField.setAccessible(true);
-                ENTITYENDERMAN_DATAWATCHER_ANGRY = dataWatcherAngryField.get(null);
-                endermanValid = true;
-            }
         }
         catch (Throwable ex) {
             ex.printStackTrace();
-            nmsWorks = false;
-            endermanValid = false;
+            nmsWorks = false; 
         }
     }
 
@@ -143,17 +105,7 @@ public class SentinelNMSHelper {
     }
 
     public static void setEndermanAngry(Enderman entity, boolean angry) {
-        if (!nmsWorks || !endermanValid) {
-            return;
-        }
-        try {
-            Object nmsHandle = CRAFTENTITY_GETHANDLE.invoke(entity);
-            Object dataWatcher = NMSENTITY_GETDATAWATCHER.invoke(nmsHandle);
-            DATWATCHER_SET.invoke(dataWatcher, ENTITYENDERMAN_DATAWATCHER_ANGRY, angry);
-        }
-        catch (Throwable ex) {
-            endermanValid = false;
-        }
+        NMS.setEndermanAngry(entity, angry);
     }
 
 
